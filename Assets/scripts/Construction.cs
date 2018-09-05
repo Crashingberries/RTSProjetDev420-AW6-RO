d713421ffdcc84f8a340922ca92a6f85099789c7
Alex Thibeault <alex96.thibeault@gmail.com>
2018-09-05 10:47:01 -0400

    Ajout Construction

---
+++ Assets/scripts/Construction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Construction : MonoBehaviour {
+    public int PV { get; set; }
+    public int Armure { get; set; }
+    public int PvMax { get; set; }
+    public int ArmureMax { get; set; }
+    public Position Pos { get; set; }
+
+    private Construction()
+    {
+        PV = 0; Armure = 0; PvMax = 0; ArmureMax = 0; Pos = new Position();
+
+    }
+    private Construction(int PvM)
+    {
+        PV = PvM; PvMax = PvM; Armure = 0; ArmureMax = 0; Pos = new Position();
+    }
+    private Construction(int PvM, int AMax)
+    {
+        PV = PvM; PvMax = PvM; Armure = AMax; ArmureMax = AMax; Pos = new Position() ;
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+}
