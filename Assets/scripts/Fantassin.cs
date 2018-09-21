e74e6c02d2422735b1ed2df1022a2bce136d1dc1
Julien <jveilletteberube@dept-info.crosemont.quebec>
2018-09-21 12:56:48 -0400

    implémentation des classes
    Une implémentation sommaire des classes qui hérite d'unité, à savoir les constructeurs avec et sans paramèrtres et les classes selon le diagramme de classes. Je n'ai pas supprimé le code préexistant et mes ajouts sont indentifiés par le commentaire «j'ai ajouté».

---
+++ Assets/scripts/Fantassin.cs
@@ -0,0 +1,18 @@
+public class Fantasin : Unité
+{
+    public Fantasin() : base()
+    {
+
+    }
+
+    public Fantassin(int unPv, int uneArmure, Position unePosition, string unNom, int unCoutOr, int unCoutBois, int uneVitesseDeplacement, int unePuissanceAttaque, int unePortéeMin, int unePortéeMax, int unePuissanceAttaque, int uneVitesseAttaque) : 
+        base(unPv, uneArmure, unePosition, unNom, unCoutOr, unCoutBois, uneVitesseDeplacement, unePuissanceAttaque, unePortéeMin, unePortéeMax, unePuissanceAttaque, uneVitesseAttaque)
+    {
+
+    }
+
+    public void Attaquer(Construction cible)
+    {
+
+    }
+}
---
+++ Assets/scripts/ouvrier.cs
@@ -0,0 +1,62 @@
+public class Ouvrier : Unité
+{
+    public static Batiment[] BatimentsAConstruire;
+	public bool EnConstruction { get; set; }
+    public bool EnReparation { get; set; }
+	public bool EnExploitation { get; set; }
+
+    //j'ai ajouté
+    public Ouvrier() : base()
+    {
+        EnConstruction = false;
+        EnReparation = false;
+        EnExploitation = false;
+    }
+
+    //j'ai ajouté
+    public Ouvrier(int unPv, int uneArmure, Position unePosition, string unNom, int unCoutOr, int unCoutBois, int uneVitesseDeplacement, int unePuissanceAttaque, int unePortéeMin, int unePortéeMax, int unePuissanceAttaque, int uneVitesseAttaque) : 
+        base(unPv, uneArmure, unePosition, unNom, unCoutOr, unCoutBois, uneVitesseDeplacement, unePuissanceAttaque, unePortéeMin, unePortéeMax, unePuissanceAttaque, uneVitesseAttaque)
+    {
+        EnConstruction = false;
+        EnReparation = false;
+        EnExploitation = false;
+    }
+
+    public Ouvrier(int PvM, int PaM, Position _pos, int _vitesse, bool _enDepl, Position _pointDepl, object _cible, int _attaque) : base(PvM, PaM, _pos, _vitesse, _enDepl, _pointDepl, _cible, _attaque)
+    {
+        PV = PvM; PvMax = PvM;
+        Armure = PaM; ArmureMax = PaM;
+        Pos = new Position();
+        Vitesse = _vitesse;
+        EnDeplacement = _enDepl;
+        EnAttaque = false;
+        PointDeplacement = _pointDepl;
+        Cible = _cible;
+        PorteeMax = 0; // nombres Non significatifs... 
+        PorteeMin = 0; //
+        Attaque = _attaque;
+        EnConstruction = false;
+        EnReparation = false;
+    }
+
+	public void Construire(string _batimentAConstruire)
+    {
+        new Batiment();
+    }
+
+	public void Reparer(Batiment _batimentAReparer)
+    {
+		// ...
+    }
+
+	public void Exploiter(RessourceNaturelle _ress)
+    {
+        // ...
+    }
+
+    //j'ai ajouté
+    public void Améliorer(Batiment unBatiment)
+    {
+        //à définir dans Unity
+    }
+}
