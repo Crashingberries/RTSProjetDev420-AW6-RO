60a514e80b5d9d317e960554863721a86049e835
Julien <jveilletteberube@dept-info.crosemont.quebec>
2018-09-26 10:27:02 -0400

    Implémentation des bâtiments, de partie, joueur et ressource

---
+++ Assets/scripts/Forge.cs
@@ -0,0 +1,18 @@
+public class Forge: Batiment
+{
+    public Forge():base()
+    {
+
+    }
+
+    public Forge(int unPV, int uneArmure, Position unePosition, string unNom, int unCoutOr, int unCoutBois, int unTempsConstruction) :
+        base(unPV, uneArmure, unePosition, unNom, unCoutOr, unCoutBois, unTempsConstruction)
+    {
+
+    }
+
+    public AmeliorerUnite(Unité uneUnite)
+    {
+
+    }
+}
