61e8ac8b49d86b1c1a128f73c3dd2d4eb6fe1967
Alex Thibeault <alex96.thibeault@gmail.com>
2018-09-28 14:49:23 -0400

    Mouvement de la caméra
    La caméra peut être déplacé par les touches up,down,left,right et par la souris sur les bords de l'écran.

---
+++ Assets/scripts/DeplacementCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacementCamera : MonoBehaviour {
+    float vitesse = 200f;
+    float limite = 10f;
+
+
+    // Use this for initialization
+    void Start () {        
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        Vector3 position = transform.position;
+
+        if (Input.GetKey("up") || Input.mousePosition.y >= Screen.height - limite)
+        {
+            position.z += vitesse * Time.deltaTime;
+        }
+        if (Input.GetKey("left") || Input.mousePosition.x <=limite)
+        {
+            position.x -= vitesse * Time.deltaTime;
+        }
+        if (Input.GetKey("down") || Input.mousePosition.y <= limite)
+        {
+            position.z -= vitesse * Time.deltaTime;
+        }
+        if (Input.GetKey("right") || Input.mousePosition.x >= Screen.width - limite)
+        {
+            position.x += vitesse * Time.deltaTime;
+        }
+
+
+        transform.position = position;
+		
+    }
+}
