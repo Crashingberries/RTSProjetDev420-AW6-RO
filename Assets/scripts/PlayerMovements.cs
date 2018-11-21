226413e36bfc64050058cdc1c392021f1c7d6f35
Julien <jveilletteberube@dept-info.crosemont.quebec>
2018-11-21 11:01:07 -0500

    backup d'assets
    j'ai somehow réussi à perdre les prefabs donc j'en fait un backup

--- Assets/scripts/PlayerMovements.cs
+++ Assets/scripts/PlayerMovements.cs
@@ -1,4 +1,4 @@
-//par Julien Veillette-Bérubé
+//par Julien Veillette-BÃ©rubÃ©
 
 using System.Collections;
 using System.Collections.Generic;
@@ -8,44 +8,76 @@ using UnityEngine;
 public class PlayerMovements : MonoBehaviour {
     [SerializeField][Range(1, 200)]
     private float vitesse = 10;
-    Animator animation;
-    Rigidbody UniteRigibody;
-    
-
     private Vector3 positionCible;
     public bool enMouvement;
+    public Animator animator;
+    public SphereCollider portee;
+    public Rigidbody self;
+    public Rigidbody cible;
 
-    const int RIGHT_MOUSE_BUTTON = 1;
+    const int CLIC_DROIT = 1;
+    const int CLIC_GAUCHE = 0;
+
+    public Rigidbody GetCible()
+    {
+        return this.cible;
+    }
+
+    public void SetCible(Rigidbody cible)
+    {
+        this.cible = cible;
+    }
 
     private void Awake()
     {
-        animation = GetComponent<Animator>();
-        UniteRigibody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        self = GetComponent<Rigidbody>();
+        portee = GetComponent<SphereCollider>();
     }
     // Use this for initialization
     void Start () {
 		positionCible = transform.position;
 		enMouvement = false;
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButton(RIGHT_MOUSE_BUTTON))
+		if (Input.GetMouseButton(CLIC_DROIT))
         {
 			SetPositionCible();
         }
-
 		if (enMouvement)
         {
 			BougerJoueur();
         }
         else
         {
-            animation.SetBool("EnMouvement", false);
+            animator.SetBool("EnMouvement", false);
         }
-	}
+        /*if (Input.GetMouseButton(0))
+        {
+            this.AttributionCible();
+        }*/
+    }
 
-	void SetPositionCible()
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name != "Terrain")
+        {
+            if (other.tag != "Friendly") {
+                animator.SetBool("Attaque", true);
+                enMouvement = false;
+                print(other.name);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        animator.SetBool("Attaque", false);
+    }
+
+    void SetPositionCible()
     {
         Plane plane = new Plane(Vector3.up, transform.position);
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -61,7 +93,7 @@ public class PlayerMovements : MonoBehaviour {
 
     void BougerJoueur()
     {
-        animation.SetBool("EnMouvement", true);
+        animator.SetBool("EnMouvement", true);
 		transform.LookAt(positionCible);
 		transform.position = Vector3.MoveTowards(transform.position, positionCible, vitesse * Time.deltaTime);
 
@@ -73,4 +105,17 @@ public class PlayerMovements : MonoBehaviour {
 		Debug.DrawLine(transform.position, positionCible, Color.red);
 
     }
+
+    /*void AttributionCible()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            if (hit.transform.gameObject.tag == "Ennemi")
+            {
+                this.SetCible(hit.transform.GetComponent<Rigidbody>());
+            }
+        }
+    }*/
 }
