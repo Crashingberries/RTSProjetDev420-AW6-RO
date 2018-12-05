4c16edc9b0beb9f0e3c163caf0af38b1b429f377
Alex Thibeault <alex96.thibeault@gmail.com>
2018-12-04 19:19:51 -0500

    Interface Fonctionnel+Serveur & Clieny SCRIPT

---
+++ Assets/scripts/MenuGameManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MenuGameManager : MonoBehaviour {
+
+    public static MenuGameManager Instance { get; set; }
+    public GameObject Menu,Creer,Rejoindre;
+
+    public GameObject serveurPrefab, clientPrefab;
+    public InputField NomClient;
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+        Instance = this;
+        Creer.SetActive(false);
+        Rejoindre.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void MenuCreerPartieBouton()
+    {
+        try
+        {
+            Serveur s = Instantiate(serveurPrefab).GetComponent<Serveur>();
+            s.Init();
+
+            Client c = Instantiate(clientPrefab).GetComponent<Client>();
+            c.NomClient = "Hote";
+            c.ConnectionServeur("127.0.0.1", 1234);
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+        Menu.SetActive(false);
+        Creer.SetActive(true);
+    }
+    public void AnnulerBouton()
+    {
+        Menu.SetActive(true);
+        Creer.SetActive(false);
+        Rejoindre.SetActive(false);
+        Serveur s = FindObjectOfType<Serveur>();
+        if (s != null) { Destroy(s.gameObject); }
+        Client c = FindObjectOfType<Client>();
+        if (c != null) { Destroy(c.gameObject); }
+    }
+    public void MenuRejoindrePartieBouton()
+    {
+        Menu.SetActive(false);
+        Rejoindre.SetActive(true);
+    }
+    public void ConnectionBouton()
+    {
+        string adresseHote = GameObject.Find("AdresseHoteInput").GetComponent<InputField>().text;
+        if (adresseHote == "")
+        {
+            adresseHote = "127.0.0.1";
+        }
+        try
+        {
+            Client c = Instantiate(clientPrefab).GetComponent<Client>();
+            c.NomClient = NomClient.text;
+            if (c.NomClient == "") { c.NomClient = "Joueur"; }
+            c.ConnectionServeur(adresseHote,1234);
+            Rejoindre.SetActive(false);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
+    }
+}
---
+++ Assets/scripts/Serveur.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+
+public class Serveur : MonoBehaviour
+{
+    public int port = 1234;
+
+    private List<ServeurClient> clients;
+    private List<ServeurClient> listedeconnection;
+
+    private TcpListener serveur;
+    private bool serveurDemarre;
+
+    public void Init()
+    {
+        DontDestroyOnLoad(gameObject);
+        clients = new List<ServeurClient>();
+        listedeconnection = new List<ServeurClient>();
+
+        try
+        {
+            serveur = new TcpListener(IPAddress.Any, port);
+            serveur.Start();
+            StartListening();
+            serveurDemarre = true;
+        }
+        catch(Exception e)
+        {
+            Debug.Log("Erreur de socket: "+e.Message);
+        }
+    }
+    private void Update()
+    {
+        if (!serveurDemarre)
+            return;
+        foreach(ServeurClient c in clients)
+        {
+            if (!EstConnecte(c.tcp))
+            {
+                c.tcp.Close();
+                listedeconnection.Add(c);
+                continue;
+            }
+            else
+            {
+                NetworkStream s = c.tcp.GetStream();
+                if (s.DataAvailable)
+                {
+                    StreamReader reader = new StreamReader(s, true);
+                    string donnees = reader.ReadLine();
+
+                    if (donnees != null)
+                    {
+                        DonneesEntrantes(c, donnees);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < listedeconnection.Count - 1; i++)
+        {
+            clients.Remove(listedeconnection[i]);
+            listedeconnection.RemoveAt(i);
+        }
+    }
+    private void StartListening()
+    {
+        serveur.BeginAcceptTcpClient(AcceptTcpClient,serveur);
+    }
+    private void AcceptTcpClient(IAsyncResult ar)
+    {
+        TcpListener listener= (TcpListener)ar.AsyncState;
+
+        ServeurClient sc = new ServeurClient(listener.EndAcceptTcpClient(ar));
+        clients.Add(sc);
+
+        StartListening();
+
+        Debug.Log("Quelqu'un s'est connecté!");
+    }
+
+    private bool EstConnecte(TcpClient c)
+    {
+        try
+        {
+            if (c != null && c.Client != null && c.Client.Connected)
+            {
+                if (c.Client.Poll(0, SelectMode.SelectRead))
+                {
+                    return !(c.Client.Receive(new byte[1], SocketFlags.Peek) == 0);
+                }
+                return true;
+            }
+            else { return false; }
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    //Envoi des données
+    private void Broadcast(string donnees, List<ServeurClient> cl)
+    {
+        foreach (ServeurClient sc in cl)
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter(sc.tcp.GetStream());
+                writer.WriteLine(donnees);
+                writer.Flush();
+            }
+            catch(Exception e)
+            {
+                Debug.Log("Erreur en ecriture: "+e.Message);
+            }
+        }
+    }
+    //Reception des données
+    private void DonneesEntrantes(ServeurClient c, string donnees)
+    {
+        Debug.Log(donnees);
+    }
+}
+
+public class ServeurClient
+{
+    public string nomClient;
+    public TcpClient tcp;
+
+    public ServeurClient(TcpClient tcp)
+    {
+        this.tcp = tcp;
+    }
+
+}
